b4e4925f56a47123a28b312b55f8c00c22682157
ja <ja>
2023-04-02 22:45:46 +0800

    http 请求转发

--- common/net/read_buffer.go
+++ common/net/read_buffer.go
@@ -4,26 +4,33 @@ import (
 	"bufio"
 	"net"
 )
-
-type ReadBufCONN struct {
+/* 包装标准库net.Conn, 组合bufio.Reader的功能*/
+type BufReader struct {
 	r *bufio.Reader
 	net.Conn
 }
 
-func NewReadBufCONN(c net.Conn)(b *ReadBufCONN){
-	b = &ReadBufCONN{
-		r: bufio.NewReader(c),
+func NewReader(c net.Conn) (b *BufReader) {
+	if b, ok := c.(*BufReader); ok {
+		return b
+	}
+	
+	b = &BufReader{
+		r:    bufio.NewReader(c),
 		Conn: c,
 	}
 	return
 }
-func (b *ReadBufCONN)Peek(n int) ([]byte, error){
+
+func (b *BufReader) Peek(n int) ([]byte, error) {
 	return b.r.Peek(n)
 }
-func (b *ReadBufCONN)Read(p []byte) (n int, err error){
+
+func (b *BufReader) Read(p []byte) (n int, err error) {
 	return b.r.Read(p)
 }
+
 // Reader returns the internal bufio.Reader.
-func (b *ReadBufCONN) Reader() *bufio.Reader {
+func (b *BufReader) Reader() *bufio.Reader {
 	return b.r
 }
--- server/mixed_server.go
+++ server/mixed_server.go
@@ -1,54 +1,79 @@
 package server
 
 import (
-	"bufio"
 	"fmt"
+	N "gohost/common/net"
 	"gohost/config"
 	"log"
 	"net"
 	"net/http"
-	"time"
-
-	N "gohost/common/net"
+	"strings"
 )
 
 var (
-	inHttp chan net.Conn
+	inHttp  chan net.Conn
 	inHttps chan net.Conn
 )
 
-func init(){
-	inHttp = make(chan net.Conn)
-	inHttps = make(chan net.Conn)
+func init() {
+	inHttp = make(chan net.Conn, 2000)
+	inHttps = make(chan net.Conn, 2000)
 }
 
-func proxyHttps(){
+func proxyHttps() {
 	for c := range inHttps {
-		reader := bufio.NewReader(c)
-		req, err:= http.ReadRequest(reader)
+		reader := N.NewReader(c)
+		req, err := http.ReadRequest(reader.Reader())
 		fmt.Printf("req: %v\n, %s", req, err.Error())
-		c.SetDeadline(time.Now())
 	}
 }
 
-func proxyHttp(){
+func proxyHttp() {
+	//todo: reuse client
+	client := &http.Client{}
+
 	for c := range inHttp {
-		reader := bufio.NewReader(c)
-		req, err:= http.ReadRequest(reader)
-		fmt.Printf("req: %v\n, %s", req, err.Error())
-		c.SetDeadline(time.Now())
+		reader := N.NewReader(c)
+		req, err := http.ReadRequest(reader.Reader())
+		if err != nil {
+			fmt.Printf("req: %v\n, %s", req, err.Error())
+			continue
+		}
+		// 读取Header中的keepAlive，判断是是否需要keepAlive
+
+
+		// 移除http请求头中的hop-by-hop headers
+		removeHopByHopHeaders(req.Header)
+		
+		// 解析req请求体，获取请求头和请求体
+
+		// reuse existing http.Request
+		req.RequestURI = ""
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("resp: %v, %s",resp, err)
+			continue
+		}
+
+		removeHopByHopHeaders(resp.Header)
+
+		// 不需要resp.Body.Close
+		// The Response Body is closed after it is sent
+		if err := resp.Write(c); err != nil{
+			fmt.Printf("resp reply error: %v, %s",resp, err)
+		}
 	}
 }
 
-
-func Setup(conf *config.Config){
-	addrPort := fmt.Sprintf("%s:%d",conf.ServerConfig.ListenAddr,conf.ServerConfig.ListenPort)
-	ln, err := net.Listen(conf.ServerConfig.Network, addrPort);
-	if  err != nil {
+func Setup(conf *config.Config) {
+	addrPort := fmt.Sprintf("%s:%d", conf.ServerConfig.ListenAddr, conf.ServerConfig.ListenPort)
+	ln, err := net.Listen(conf.ServerConfig.Network, addrPort)
+	if err != nil {
 		log.Fatalf("启动监听失败: %s", err.Error())
-		
+
 	}
-	log.Printf("服务启动成功： %s",addrPort)
+	log.Printf("服务启动成功： %s", addrPort)
 
 	go proxyHttp()
 	go proxyHttps()
@@ -62,10 +87,10 @@ func Setup(conf *config.Config){
 	}
 }
 
-func handleConnWithProtocol(conn net.Conn){
+func handleConnWithProtocol(conn net.Conn) {
 	//todo: 判断代理协议
-	
-	bConn := N.NewReadBufCONN(conn)
+
+	bConn := N.NewReader(conn)
 	head, err := bConn.Peek(1)
 	if err != nil {
 		log.Printf("接受代理请求失败：%s", err.Error())
@@ -79,18 +104,40 @@ func handleConnWithProtocol(conn net.Conn){
 			log.Printf("接受CONNECT请求失败：%s", err.Error())
 		}
 
-		if _, err := fmt.Fprintf(conn, "HTTP/%d.%d %03d %s\r\n\r\n", req.ProtoMajor, req.ProtoMinor, http.StatusOK, "Connection established"); err != nil{
+		// 发送接受代理响应
+		if _, err := fmt.Fprintf(conn, "HTTP/%d.%d %03d %s\r\n\r\n", req.ProtoMajor, req.ProtoMinor, http.StatusOK, "Connection established"); err != nil {
 			log.Printf("发送接受代理请求失败：%s", err.Error())
 			return
 		}
 
-		// inHttps <- conn
-	}else{
+		inHttps <- bConn
+	} else {
 		// http 请求
-		// inHttp <- conn
-		req, err:= http.ReadRequest(bConn.Reader())
-		fmt.Printf("req: %v\n, %s", req, err.Error())
+		inHttp <- bConn
+		log.Printf("接受http代理请求: %s --> %s",bConn.RemoteAddr().String(), bConn.LocalAddr().String())
 	}
 
+}
 
+// 移除http请求头中的hop-by-hop headers 
+// Hop-by-hop headers 是 HTTP 协议中的一种头部，用于控制在传输过程中每个单独的代理或网关所需执行的操作。这些头部只对当前经过的单个节点有效，并且不会被转发到下一个节点。常见的 hop-by-hop headers 包括 Connection、Keep-Alive、TE（用于传输编码）、Trailer（用于指定消息尾部的字段列表）等
+func removeHopByHopHeaders(header http.Header) {
+	hopHeaders := []string{
+		"TE",
+		"Trailer",
+		"Transfer-Encoding",
+		"Upgrade",
+		"Proxy-Authenticate",
+		"Proxy-Authorization",
+		"Proxy-Connection",
+	}
+	for _, hk := range hopHeaders {
+		header.Del(hk)
+	}
+	connHeader := header.Get("Connection")
+	for _, h := range strings.Split(connHeader, ",") {
+		h = strings.TrimSpace(h)
+		header.Del(h)
+	}
+	header.Del("Connection")
 }
